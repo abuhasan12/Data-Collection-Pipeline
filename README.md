59a6ccd611f9a2bfdff3499a9e3a4e5add1e702a
unknown <abu_h@live.co.uk>
2022-06-03 18:34:02 +0100

    created functions to retrieve all data and store in files in working directory

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # Data-Collection-Pipeline
 
-## Milestone 1 - Decide website
+This project an end-to-end data collection pipeline that scrapes the details of all laptops on the Currys.co.uk website and saves the resulting dataset to a database. The dataset can be used to perform any data analysis or as part of a data-science project to recommend the best laptop for a use-case.
 
-This scraper will scrape product details from the Currys website
+## Requirements
 
-## Milestone 2 - Prototype finding the individual page for each entry
+* Python
 
-I have created a scraper class that contains various methods to perform the function of webscraping. The webscraping is done using selenium chromedriver to navigate the web browser. Each instance of the class is initialised with the Currys homepage as the base URL. There are methods to accept cookies and perform a search based on the search input that the user picks. Once the search is performed, the program will go through each page of the page results and retrieve an individial link for each result into a list.
+## Installation
+
+```python
+pip install -r requirements.txt
+```
+
+## Run
+
+```python
+python currys_scraper.py
+```
+
+## Finding each laptop page
+
+The `CurrysLaptopScraper` class uses a Selenium Chromedriver instance to launch the Currys homepage. Selenium is originally used for running test scripts for test automation. However, it makes a great candidate for web-scraping because it navigates through JavaScript object well. The first step it takes is accepting cookies before navigating to the page for all laptop results using the relevant URL. Once it has reached this page, the scraper will set the number of results per page to 50 so that it won't have to search through more than double the pages that's needed (the default is set to 20). The scraper will then begin to retrieve the individual URLs for each laptop listed on the page before moving to the next page and doing the same, until there are no more to retrieve. The URLs are stored in a Python list.
+
+## Retrieving data for each laptop
+
+Next, the scraper begins to collect details from each page in the Python list of URLs. Before it does that though, a UUID is generated for each laptop. All the details are saved in a python dictionary called `attributes`. The first key-value pair stored is the product code for each individual laptop. Then in order - `title`, `image`, `price`, `rating` (if present), `ratingCount` (if present) and specifcations. The specifications are retrieved when the scraper's selenium webdriver scrolls down the page and clicks on the specifcations section. Once the python dictionary is made for each laptop, the dictionary is saved to a json file in the working directory in a folder called `raw_data`. Each laptop page has its own entry with a `data.json` file containing all the key-value pairs, as well as an image file corresponding the laptop.
--- currys_scraper.py
+++ currys_scraper.py
@@ -1,17 +1,25 @@
+from email import header
+import requests
+import json
+import os
 import time
+import uuid
+from attr import attr
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 
+# go through and ask urself if i can understand from whats happening or do i need to make variables and document
+# change to laptop scraper
 # browsing?
 # a way to exit while loops...
 # for some reason, next page wasnt clicking when i searched for 'search' - think cos of lag I also got this selenium.common.exceptions.StaleElementReferenceException: Message: stale element reference: element is not attached to the page document
+# the other error is the other element would receive click one for pop up - Tell us what you think. selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Other element would receive the click: 
 
-
-class CurrysScraper:
+class CurrysLaptopScraper:
     def __init__(self):
         self.base_url = "https://www.currys.co.uk/"
-        self.chromedriver_path = "C:/Users/Abu Hasan/chromedriver102/chromedriver_win32/chromedriver"
+        self.chromedriver_path = "./chromedriver102/chromedriver_win32/chromedriver" #insert your path to the chromedriver here
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -21,32 +29,26 @@ class CurrysScraper:
     
     def create_session(self):
         self.driver.get(self.base_url)
+        time.sleep(1)
         self.driver.delete_all_cookies()
-        time.sleep(2)
 
     def accept_cookies(self):
+        time.sleep(1)
         buttons = self.driver.find_elements_by_xpath('//button')
         for button in buttons:
             if "accept" in button.text.lower() and "cookies" in button.text.lower():
                 accept_cookies_button = button
         accept_cookies_button.click()
-        time.sleep(1)
     
     def perform_search(self):
-        while True:
-            self.search_word = input("What item would you like to search for?")
-            search_box = self.driver.find_element_by_xpath('//*[@id="Search"]')
-            search_box.send_keys(self.search_word)
-            time.sleep(1)
-            search_box.send_keys(Keys.ENTER)
-            time.sleep(2)
-            try:
-                self.driver.find_element_by_xpath('//*[contains(@id, "select2-sort-pagination")]').click()
-                break
-            except NoSuchElementException:
-                print("Please try narrowing your search.")
         time.sleep(1)
-        self.driver.find_element_by_xpath('//option[@data-id="50"]').click()
+        self.driver.get(self.base_url + "computing/laptops/laptops")
+        time.sleep(2)
+        results_count = self.driver.find_element_by_xpath('//*[contains(@id, "select2-sort-pagination")]')
+        results_count.click()
+        time.sleep(1)
+        results_count_50 = self.driver.find_element_by_xpath('//option[@data-id="50"]')
+        results_count_50.click()
     
     def get_search_result_links(self):
         self.search_result_links = set([])
@@ -56,61 +58,138 @@ class CurrysScraper:
             for link in links:
                 self.search_result_links.add(link.get_attribute('href'))
             try:
+                time.sleep(1)
                 next_page_button = self.driver.find_element_by_xpath('//a[@class="updateGrid page-next page-link"]')
+                self.driver.execute_script("arguments[0].scrollIntoView();", next_page_button)
+                time.sleep(1)
                 next_page_button.click()
             except NoSuchElementException:
                 break
         self.search_result_links = list(self.search_result_links)
+
+    def get_product_code(self):
+        product_code = self.driver.find_element_by_xpath('//*[@class="product-code"]').text
+        return product_code[product_code.find(":")+2 :]
     
-    # def browse_or_search_option(self):
-    #     select_browse_or_search = "not selected"
-    #     while select_browse_or_search not in {"browse", "search"}:
-    #         select_browse_or_search = input("Would you like to search for a specific item/keyword, or browse the website?").lower()
-    #         if "browse" in select_browse_or_search:
-    #             self.browse_or_search = "browse"
-    #         elif "search" in select_browse_or_search or "item" in select_browse_or_search or "keyword" in select_browse_or_search:
-    #             self.browse_or_search = "search"
+    def get_title(self):
+        title = self.driver.find_element_by_xpath('//h1[@class="product-name"]').text
+        return title
+    
+    def get_image(self):
+        image_source =  self.driver.find_element_by_xpath('//div[@class="carouselitem active"]/a/img').get_attribute('src')
+        return image_source
+    
+    def get_price(self):
+        price = self.driver.find_elements_by_xpath('//span[@class="value"]')[1].text[1:]
+        return price
+    
+    def get_rating(self):
+        try:
+            rating = self.driver.find_element_by_xpath('//span[@class="star-ratings"]').get_attribute('title')
+            return rating[0:rating.find(' ')]
+        except Exception as e:
+            print("Failed 1")
+            return None
+    
+    def get_rating_count(self):
+        try:
+            rating_count = self.driver.find_element_by_xpath('//span[@class="rating-count"]/a').text[1:-1]
+            return rating_count
+        except Exception as e:
+            print("Failed 2")
+            return None
+    
+    def get_specs(self):
+        specs = self.driver.find_elements_by_xpath('//div[@class="tech-specification-body"]/div')
+        return specs
+    
+    def get_spec_key(self, spec):
+        spec_key = spec.find_element_by_xpath('.//div[@class="tech-specification-th col-6 col-lg-4"]').text
+        return spec_key
+    
+    def get_spec_value(self, spec):
+        spec_value = spec.find_element_by_xpath('.//div[@class="tech-specification-td col-6 col-lg-8"]').text
+        return spec_value
+    
+    def scrape(self, laptop_link):
+        time.sleep(1)
+        self.driver.get(laptop_link)
+        time.sleep(2)
+        attributes = {}
+        attributes['uuid'] = str(uuid.uuid4())
+        attributes['href'] = laptop_link
+        attributes['id'] = self.get_product_code()
+        attributes['title'] = self.get_title()
+        attributes['price'] = self.get_price()
+        attributes['imageSource'] = self.get_image()
+        time.sleep(1)
+        attributes['rating'] = self.get_rating()
+        attributes['ratingCount'] = self.get_rating_count()
+        time.sleep(1)
+        product_information_button = self.driver.find_element_by_xpath('//*[@id="accordion"]/div[1]/div/a')
+        self.driver.execute_script("arguments[0].scrollIntoView();", product_information_button)
+        time.sleep(1)
+        specifications_button = self.driver.find_element_by_xpath('//*[@id="accordion"]/div[2]/div/a')
+        specifications_button.click()
+        specs = self.get_specs()
+        for spec in specs:
+            spec_key = self.get_spec_key(spec).title().replace(" ", "")
+            spec_key = spec_key[0].lower() + spec_key[1:]
+            spec_value = self.get_spec_value(spec)
+            attributes[spec_key] = spec_value
+        return attributes
+    
+    def save_scraped(self, attributes):
+        raw_data_path = "raw_data/"
+        folder_path = raw_data_path + attributes['id']
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            with open(os.path.join(folder_path, 'data.json'), 'w') as fp:
+                json.dump(attributes, fp)
+            image_file_name = attributes['id'] + '.jpg'
+            request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'}
+            image_request = requests.get(attributes['imageSource'], headers=request_headers)
+            with open(os.path.join(folder_path, image_file_name), 'wb') as f:
+                f.write(image_request.content)
+        else:
+            print(f"Folder already exists for {attributes['id']}")
 
-    # def get_categories(self):
-    #     self.category_names = []
-    #     categories = self.driver.find_elements_by_xpath('/html/body/wainclude/div[1]/div[1]/header/section/div/div/div[2]/div[2]/div[1]/nav/div/ul/li/a')
-    #     for category in categories:
-    #         self.category_names.append(category.text)
 
-    # def select_category(self):
-    #     category = "not selected"
-    #     while category not in self.category_names:
-    #         category = input(f"Please select from one of the following categories: {self.category_names}").lower()
-    #         if "tv" in category or "audio" in category:
-    #             category = "TV & Audio"
-    #         elif "home" in category or "outdoor" in category:
-    #             category = "Home & Outdoor"
-    #         else:
-    #             category = category.title()
-    #     self.category = category
-
-    # def get_sub_categories(self):
-    #     self.sub_category_names = []
-    #     self.sub_categories = self.driver.find_elements_by_xpath('')
-
-    # def select_sub_category(self):
-    #     pass
-
-    # def get_filters(self):
-    #     pass
-
-    # def get_filter_options(self):
-    #     pass
+    def scrape_all(self):
+        for result_link in self.search_result_links:
+            scraped_attributes = self.scrape(result_link)
+            self.save_scraped(scraped_attributes)
+    
+    def directory_check(self):
+        raw_data_path = 'raw_data'
+        if not os.path.exists(raw_data_path):
+            os.makedirs(raw_data_path)
+    
+    def click_popup(self):
+        buttons = self.driver.find_elements_by_xpath('//button')
+        for button in buttons:
+            if "accept" in button.text.lower() and "cookies" in button.text.lower():
+                accept_cookies_button = button
+        accept_cookies_button.click()
 
     def end_session(self):
         self.driver.quit()
 
+
 if __name__ == '__main__':
-    currys_scraper = CurrysScraper()
-    currys_scraper.create_session()
-    currys_scraper.accept_cookies()
-    currys_scraper.perform_search()
-    currys_scraper.get_search_result_links()
-    currys_scraper.driver.get(currys_scraper.search_result_links[1])
-    time.sleep(10)    
-    currys_scraper.end_session()
+    laptop_scraper = CurrysLaptopScraper()
+    laptop_scraper.create_session()
+    laptop_scraper.accept_cookies()
+    laptop_scraper.perform_search()
+    laptop_scraper.get_search_result_links()
+    laptop_scraper.directory_check()
+    first_forty = laptop_scraper.search_result_links[0:41]
+    for each in first_forty:
+        try:
+            laptop_attributes = laptop_scraper.scrape(each)
+        except ElementClickInterceptedException:
+            laptop_scraper.click_popup()
+            laptop_attributes = laptop_scraper.scrape(each)
+        laptop_scraper.save_scraped(laptop_attributes)
+    time.sleep(10)
+    laptop_scraper.end_session()
